6772c33fcc5d22ff274b30d69b25b958b7f3d209
danielasserra <dany3317@gmail.com>
2022-03-16 10:29:36 -0300

    Primera version

---
+++ TP 2/variables.js
@@ -0,0 +1,21 @@
+//declaración y asignación de variables
+var cantidadDeVentanasEnMiCasa = 4;
+var soyHumano = true;
+var gustoDePizzaPreferido = 'Napolitana';
+var random = false;
+
+//mostrar en consola
+console.log('Cantidad de ventanas en mi casa ' + cantidadDeVentanasEnMiCasa);
+console.log('soy humano ' + soyHumano);
+console.log('Mi gusto de pizza preferido es ' + gustoDePizzaPreferido)
+console.log(random)
+
+//cambio valor variable random
+var random = 'Hola'
+console.log(random)
+
+var random = 0
+console.log(random)
+
+var random = 'Minions'
+console.log(random)
