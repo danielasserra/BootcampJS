2b3d0b7c4eae090f1e1536572f19873e092b5324
danielasserra <dany3317@gmail.com>
2022-03-16 16:29:17 -0300

    First commit tp3 - 1

---
+++ TP 3 - 1/codigo.js
@@ -0,0 +1,81 @@
+//ejercicio 1
+var noValgoNi5 = 4;
+
+console.log(noValgoNi5);
+
+//ejercicio 2
+var miAnioDeNacimiento = 1987;
+var meGustariaTener80Anios = 80;
+var voyATener80ElAnio = miAnioDeNacimiento + meGustariaTener80Anios;
+
+console.log(miAnioDeNacimiento);
+console.log(voyATener80ElAnio);
+
+//ejercicio 3
+var costoDeUnaTele = 10000;
+var ahorros = 9000;
+var dineroQueMeFalta = costoDeUnaTele - ahorros;
+
+console.log(dineroQueMeFalta);
+
+//ejercicio 4
+var diasPorAño = 365;
+var cantidadDeAñosEnUnaDecada = 10;
+var cantidadDeDiasEnUnaDecada = diasPorAño * cantidadDeAñosEnUnaDecada;
+
+console.log(cantidadDeDiasEnUnaDecada);
+
+//ejercicio 5
+var PorcionesDisponibles = 24
+const PorcionesPorPizza = 8
+var PizzasDisponibles = PorcionesDisponibles / PorcionesPorPizza;
+
+console.log(PizzasDisponibles);
+
+//Ejercicio 6
+var miAnimalFavorito = 'Gato';
+const animal = 'Perro';
+var animalFavoritoPerro = miAnimalFavorito == animal;
+
+console.log(animalFavoritoPerro);
+
+//Ejercicio 7
+console.log('flan' != 'flan con dulce de leche');
+
+var postreA = 'flan';
+var postreB = 'flan con ddl';
+var postreCoincide = postreA != postreB;
+
+console.log(postreCoincide);
+console.log(postreA != postreB);
+
+//Ejercicio 8
+var MiEdad = 34;
+var soyMayordeEdad = MiEdad>18;
+console.log(soyMayordeEdad);
+
+//Ejercicio 9
+var soyMenorde25 = MiEdad<25;
+console.log(soyMenorde25);
+
+//Ejercicio 10
+var mesActual = 'Marzo';
+var mesEnero = 'Enero'
+var estamosEnEnero = mesActual === mesEnero;
+console.log(estamosEnEnero);
+
+//Ejercicio 11
+var noEstamosEnEnero = mesActual !== mesEnero;
+console.log(noEstamosEnEnero);
+
+//Ejercicio 12
+var miNotaEnElParcial = 8;
+var notaMinimaParaAprobar = 6;
+var estoyAprobado = miNotaEnElParcial >= notaMinimaParaAprobar;
+console.log(estoyAprobado);
+
+//ejercicio 13
+var horaActual = 16.24;
+var horaDeAlmorzar = 12;
+var esHoraDeAlmorzar = horaActual <= horaDeAlmorzar;
+console.log(esHoraDeAlmorzar);
